d7ce4aa72a16ea4f6b0a243138edfc0d80a02f0b
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2024-11-10 00:47:35 +0530

    fixed that extra link listing on index page and that image is required while listing

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "public")));
 
-const MONGO_URL = "mongodb://127.0.0.1:27017/celeste";
+// const MONGO_URL = "mongodb://127.0.0.1:27017/celeste";
 const dbUrl = process.env.ATLASDB_URL;
 
 const store = MongoStore.create({
