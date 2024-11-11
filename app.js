0ae25033320a7f490749c50b7d5df45161bf3eca
SubhodeepSamanta <subhodeepsamanta2005@gmail.com>
2024-11-11 13:50:41 +0530

    Added Stripe Feature and added cursor effect on main page

--- app.js
+++ app.js
@@ -57,6 +57,53 @@ app.get("/", async (req, res) => {
   res.redirect("/listings");
 });
 
+
+//payment wali bakchodi
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
+app.post('/checkout', async (req, res) => {
+  const session = await stripe.checkout.sessions.create({
+      line_items: [
+          {
+              price_data: {
+                  currency: 'usd',
+                  product_data: {
+                      name: 'HandBag'
+                  },
+                  unit_amount: 1770 * 100
+              },
+              quantity: 1
+          },        
+      ],
+      mode: 'payment',
+      shipping_address_collection: {
+          allowed_countries: ['US', 'BR']
+      },
+      success_url: `${process.env.BASE_URL}/complete?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.BASE_URL}/cancel`
+  })
+
+  res.redirect(session.url)
+})
+
+app.get('/complete', async (req, res) => {
+  const result = Promise.all([
+      stripe.checkout.sessions.retrieve(req.query.session_id, { expand: ['payment_intent.payment_method'] }),
+      stripe.checkout.sessions.listLineItems(req.query.session_id)
+  ])
+
+  console.log(JSON.stringify(await result))
+
+  res.send('Your payment was successful')
+})
+
+app.get('/cancel', (req, res) => {
+  res.redirect('/')
+})
+
+
+//all other stuff
+
 app.use(session(sessionOptions));
 app.use(flash());
 
